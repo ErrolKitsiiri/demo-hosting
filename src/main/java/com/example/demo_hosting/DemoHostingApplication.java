08ff2c37f62a87ed01bf0cc0dd5995b2b9cf4557
ErrolKitsiiri <errolantonio325@gmail.com>
2025-06-08 13:35:18 +0300

    first commit

---
+++ src/main/java/com/example/demo_hosting/DemoHostingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_hosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHostingApplication.class, args);
+	}
+
+}
